4728d2b276279b7c2c7035e7829e4854db64b622
Jonescifer <wolf_paul@icloud.com>
2020-07-29 18:16:22 +0300

    my take on methods login register..

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -1,50 +1,61 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 
 namespace OperatorsC
 {
      class MainClass
     {
+        static string newUsername = "";
+        static string newPassword = "";
+        static string username = "";
+        static string password = "";
+        
         public static void Main(string[] args)
         {
-            
-            string newUsername = "";
-            string newPassword = "";
-            string username = "";
-            string password = "";
-            
-            
+            Register();
+        }
+
+        public static void Register()
+        {
             Console.WriteLine("Registration screen.");
             Console.WriteLine("Enter new Username: ");
             newUsername = Console.ReadLine();
             Console.WriteLine("Enter new Password: ");
             newPassword = Console.ReadLine();
-            
+            Console.WriteLine("Registration ended.");
+            Console.WriteLine("------------------------------------");
+
             if (newUsername == "" || newPassword == "")
             {
-                Console.WriteLine("username or password cant be empty!");
+                Console.WriteLine("Failed to register: username or password cant be empty!");
             }
             else
             {
-                Console.WriteLine("Login screen. ");
-                Console.WriteLine("Enter Username: ");
-                username = Console.ReadLine();
-                Console.WriteLine("Enter Password: ");
-                password = Console.ReadLine();
-                
-                if (username == "" || password == "")
-                {
-                    Console.WriteLine("username or password cant be empty!");
-                }
-                else if (username == newUsername && password == newPassword)
-                {
-                    Console.WriteLine("welcome {0}", username);
-                }
-                else
-                {
-                    Console.WriteLine("Wrong username/password combination, you are an idiot stimpy!");
-                }
+                Login();
             }
-            
         }
+
+        public static void Login()
+        {
+            Console.WriteLine("Login screen. ");
+            Console.WriteLine("Enter Username: ");
+            username = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            password = Console.ReadLine();
+                
+            if (username == "" || password == "")
+            {
+                Console.WriteLine("Failed to login: username or password cant be empty!");
+            }
+            else if (username == newUsername && password == newPassword)
+            {
+                Console.WriteLine("welcome {0}", username);
+            }
+            else
+            {
+                Console.WriteLine("Wrong username/password combination, you are an idiot stimpy!");
+            }
+        }
+
     }
 }
