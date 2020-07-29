56e6fc3afc9a760768008fa12786101751be5423
Jonescifer <wolf_paul@icloud.com>
2020-07-29 15:22:00 +0300

    CheckTemperture(AskUser());

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -6,63 +6,32 @@ namespace OperatorsC
     {
         public static void Main(string[] args)
         {
-            //unary operators
-            int num1 = 5;
-            int num2 = 3;
-            int num3 = -num1;
-            Console.WriteLine("num3 is: {0}",num3);
-            Console.WriteLine("num1 is: {0}",num1);
-            Console.WriteLine("num2 is: {0}",num2);
+            CheckTemperture(AskUser());
+        }
 
-            bool isSunny = true;
-            Console.WriteLine("Is it sunny? {0}" , !isSunny);
+        static int AskUser()
+        {
+            Console.WriteLine("please enter temperature outside!");
+            int inputTemperature = int.Parse(Console.ReadLine());
+            return inputTemperature;
+        }
+        
+        static void CheckTemperture(int temperture)
+        {
             
-            //increment operators
-            
-            int num = 0;
-            num++;
-            Console.WriteLine("the number is: {0}", num);
-            Console.WriteLine("the number is: {0}", num++); // post increment
-            Console.WriteLine("the number is: {0}", ++num); //pre increment
-            
-            //decrement
-            num--;
-            Console.WriteLine("the number is: {0}", num);
-            Console.WriteLine("the number is: {0}", num--); // post deccrement
-            Console.WriteLine("the number is: {0}", --num); //pre deccrement
-
-            int result;
-
-            result = num1 + num2;
-            Console.WriteLine("result of num1 + num2 is : {0}", result);
-            
-            result = num1 - num2;
-            Console.WriteLine("result of num1 - num2 is : {0}", result);
-            
-            result = num1 / num2;
-            Console.WriteLine("result of num1 divided by num2 is : {0}", result);
-            
-            result = num1 * num2;
-            Console.WriteLine("result of num1 multiplied num2 is : {0}", result);
-            
-            result = num1 % num2;
-            Console.WriteLine("result of num1 % num2 is : {0}", result);
-            
-            bool isLower = num1 < num2;
-            Console.WriteLine("num1 < num2 is : {0}", isLower);
-            
-            bool isEqual = num1 == num2;
-            Console.WriteLine("num1 equals num2 is : {0}", isEqual);
-            
-            bool isNotEqual = num1 != num2;
-            Console.WriteLine("num1 Not equals num2 is : {0}", isNotEqual);
-
-            bool isLowerAndSunny = isLower && isSunny;
-            Console.WriteLine("is it lower and sunny? : {0}", isLowerAndSunny);
-
-            bool isLowerOrSunny = isLower || isSunny;
-            Console.WriteLine("is it lower or sunny? : {0}", isLowerOrSunny);
-
+            if (temperture < 20)
+            {
+                Console.WriteLine("stay home");
+            }
+            else if (temperture == 20)
+            {
+                Console.WriteLine("put coat and go outside");
+            }
+            else
+            {
+                Console.WriteLine("Go to the Beach");
+            }
+                
         }
         
     }
