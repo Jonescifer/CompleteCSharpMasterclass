1cb1f87bbc6b022aefb6f05445943d0bb8dafb22
Jonescifer <wolf_paul@icloud.com>
2020-07-31 17:34:09 +0300

    people counter (show one more in while loop..fixed with writeconsole ounter -=1)

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -7,31 +7,21 @@ namespace CompleteCSharpMasterclass
     {
         public static void Main(string[] args)
         {
-            CheckTemperature(UserInput());
-        }
 
-        static string UserInput()
-        {
-            Console.WriteLine("enter temperature:");
-            string userInput = Console.ReadLine();
-            return userInput;
-        }
-        static void CheckTemperature(string useInput )
-        { 
-            string feeling;
             
-            //validate the input as a valid integer value
-            bool validInt = int.TryParse(useInput, out int intTemperature);
-           
-            if(validInt)
-            { 
-                feeling = intTemperature <= 15 ? "its to cold here" : intTemperature >= 16 && intTemperature <= 28  ? "its ok here" : "its to hot here";
-               Console.WriteLine(feeling); 
-            }
-            else 
+            //my take
+            int studentCounter = 0;
+            string enteredText = "";
+            
+            while (enteredText.Equals (""))
             {
-               Console.WriteLine("Not a valid temperature value, bianca."); 
-            }
-        }
+                Console.WriteLine("press Enter");
+                enteredText = Console.ReadLine();
+                studentCounter++;
+                Console.WriteLine("current student number is : {0}" , studentCounter);
+            } 
+            Console.Write((studentCounter -= 1 ) + " students entered");
+        } 
+           
     }
 }
