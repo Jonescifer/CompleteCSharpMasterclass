5a29fd5049bc54fc7c9c53f96cb273bc089846e2
Jonescifer <wolf_paul@icloud.com>
2020-07-31 14:25:23 +0300

    highscore

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -1,74 +1,36 @@
 ﻿using System;
 using System.Diagnostics.Eventing.Reader;
 
-namespace OperatorsC
+namespace CompleteCSharpMasterclass
 {
     class MainClass
     {
-        static string username = "";
-        static string password = "";
-
+        static int highScore = 20;
+        static string highScorePlayer = "Josh";
+       
         public static void Main(string[] args)
         {
-            Register();
+            CheckHighScore(19,"Jones");
+            CheckHighScore(21,"Josh");
         }
 
-        public static void Register()
+        static void CheckHighScore(int score, string playerName)
         {
-            Console.WriteLine("Registration screen.");
-            Console.WriteLine("Enter new Username: ");
-            username = Console.ReadLine();
-            
-            //check username is not empty
-            if (username != "")
+            if (score > highScore)
             {
-                Console.WriteLine("Enter new Password: ");
-                password = Console.ReadLine();
-                 
-                //check password is not empty
-                if (password != "")
-                {
-                    Console.WriteLine("Registration ended.");
-                    Console.WriteLine("------------------------------------");
-                    
-                    //run method Login
-                    Login();
-                }
-                else
-                {
-                    Console.WriteLine("Failed to register: password cant be empty!"); 
-                }
+                 highScore = score;
+                 highScorePlayer = playerName;
+                 Console.WriteLine("New high score! High score is: {0}, by {1}", highScore,highScorePlayer);
             }
             else
+
             {
-                Console.WriteLine("Failed to register: username cant be empty!");
+                Console.WriteLine("No new high score. High score is still: {0}, by {1}", highScore,highScorePlayer);
+                
             }
             
         }
 
-        public static void Login()
-        {
-            Console.WriteLine("Login screen. ");
-            Console.WriteLine("Enter Username: ");
-                        
-            if (username == Console.ReadLine())
-            { 
-                Console.WriteLine("Enter Password: ");
-                if (password == Console.ReadLine()) 
-                { 
-                    Console.WriteLine("Login Successful {0}!", username);
-                }
-                else
-                { 
-                    Console.WriteLine("Wrong password, you are an idiot stimpy!");
-                }
-            }
-            else 
-            { 
-                Console.WriteLine("Wrong username, you are an idiot stimpy!");
-            }
-
-        }
     }
 }
 
