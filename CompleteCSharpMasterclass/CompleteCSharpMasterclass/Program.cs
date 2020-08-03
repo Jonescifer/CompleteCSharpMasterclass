bd6103b05949e6f1acc0c5ba9da65b58c7a905f2
Jonescifer <wolf_paul@icloud.com>
2020-08-03 08:24:47 +0300

    ww

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -8,28 +8,7 @@ namespace CompleteCSharpMasterclass
     {
         public static void Main(string[] args)
         {
-            // creat an instance of Class Human
-            Human paul = new Human("Paul" , "Wolf", "Blue",41);
-            Human maul = new Human("maul" , "rolf", "grey");
-            Human saul = new Human("saul" , "dolf");
-            Human laul = new Human("laul");
-            Human kaul = new Human("kaul" , "lolf",41);
-            Human defaulHuman = new Human();
-            /*
-            //access public variable and change it.
-            paul.firstName = "Paul";
-            paul.lastName = "Wolf";
-            */
             
-            //call method of the Class
-            paul.IntroduceMyself();
-            maul.IntroduceMyself();
-            saul.IntroduceMyself();
-            laul.IntroduceMyself();
-            kaul.IntroduceMyself();
-
-
-
         }
     }
 }
