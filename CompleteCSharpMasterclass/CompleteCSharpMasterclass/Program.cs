e81fe411e7802594c20bd210241e12fac23d30f1
Jonescifer <wolf_paul@icloud.com>
2020-08-12 16:47:13 +0300

    finished tick tack toe - the checkWinStatus is CUMBERSOME! must find cleaner way.

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -8,119 +8,122 @@ namespace CompleteCSharpMasterclass
 {
     class Program
     {
-        static bool _playerOneWin = false;
-        static bool _playerTwoWin = false;
-        static bool _gameWon = false;
+        static bool _gameWon;
+        private static bool _gameDraw = false;
         static int _currentPlayer = 1; // at the beginning the first player is 1
+
         static string[,] arrayTicTacToe =
-                    {
-                        {" "," "," ","|"," "," "," ","|"," "," "," "},
-                        {" ","1"," ","|"," ","2"," ","|"," ","3"," "},
-                        {"_","_","_","|","_","_","_","|","_","_","_"},
-                        {" "," "," ","|"," "," "," ","|"," "," "," "},
-                        {" ","4"," ","|"," ","5"," ","|"," ","6"," "},
-                        {" "," "," ","|"," "," "," ","|"," "," "," "},
-                        {"_","_","_","|","_","_","_","|","_","_","_"},
-                        {" "," "," ","|"," "," "," ","|"," "," "," "},
-                        {" ","7"," ","|"," ","8"," ","|"," ","9"," "},
-                        {" "," "," ","|"," "," "," ","|"," "," "," "}
-                    };
-        
-        
+        {
+            //0    1    2    3    4    5    6    7    8    9  
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //0
+            {" ", "1", " ", "|", " ", "2", " ", "|", " ", "3"}, //1
+            {"_", "_", "_", "|", "_", "_", "_", "|", "_", "_"}, //2
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //3
+            {" ", "4", " ", "|", " ", "5", " ", "|", " ", "6"}, //4
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //5
+            {"_", "_", "_", "|", "_", "_", "_", "|", "_", "_"}, //6
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //7
+            {" ", "7", " ", "|", " ", "8", " ", "|", " ", "9"}, //8
+            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "} //9
+        };
+
+
         public static void Main(string[] args)
         {
             ShowGame(arrayTicTacToe);
             RunGame();
-            
+
         }
+
         static void ShowGame(string[,] ticTacToe)
         {
             for (var i = 0; i < ticTacToe.GetLength(0); i++)
-            { 
-                for (var j = 0; j < ticTacToe.GetLength(1); j++)  
-                { 
-                    Console.Write("{0}" , ticTacToe[i,j]);
-                } 
+            {
+                for (var j = 0; j < ticTacToe.GetLength(1); j++)
+                {
+                    Console.Write("{0}", ticTacToe[i, j]);
+                }
+
                 Console.WriteLine();
             }
         }
+
         static void RunGame()
         {
-            while (!_gameWon)
+            while (!_gameWon || !_gameDraw)
             {
-                CheckUserInput(); 
-                if (_playerOneWin || _playerTwoWin)
-                { 
-                    Console.WriteLine("Player {0} has won!" , _currentPlayer ); 
+                CheckGameStatus();
+                Console.Clear();
+                ShowGame(arrayTicTacToe);
+                if (_gameWon)
+                {
+                    Console.WriteLine("Player {0} has won!", _currentPlayer);
+                    break;
+                }
+
+                if (_gameDraw)
+                {
+                    Console.WriteLine("Its a draw!");
                     break;
                 }
             }
         }
-        
-        static void CheckUserInput()
+
+        static void CheckGameStatus()
         {
             //ask user for input.
             Console.WriteLine("Player {0}: Choose your field!", _currentPlayer);
-            
+
             // check if is a number and return it.
             var isNumeric = int.TryParse(Console.ReadLine(), out int intPlayerInput);
 
-            if (isNumeric && intPlayerInput > 0 && intPlayerInput <= 9 )
+            if (isNumeric && intPlayerInput > 0 && intPlayerInput <= 9)
             {
                 switch (_currentPlayer)
                 {
-                    case (1):
+                    case 1:
                         EditArray(intPlayerInput);
-                        CheckWinStatus(); //must set _gameWon to true if player wins
-                        if (_gameWon)
-                        {
-                            break;
-                        }
+                        CheckWinStatus();
                         _currentPlayer = 2;
                         break;
-                    case(2):
+                    case 2:
                         EditArray(intPlayerInput);
-                        CheckWinStatus(); //must set _gameWon to true if player wins
-                        if (_gameWon)
-                        {
-                            break;
-                        }
+                        CheckWinStatus();
                         _currentPlayer = 1;
                         break;
                 }
             }
-            //wrong input entered
-            Console.WriteLine("Please enter a valid number between 1 and 9!");
+            else
+            {
+                //wrong input entered
+                Console.WriteLine("Please enter a valid number between 1 and 9!");
+                Console.ReadKey();
+            }
+
         }
 
         static void EditArray(int intPlayerInput)
-        
+
         {
             string mark = "";
-            
+
             switch (_currentPlayer)
             {
-                case(1):
+                case 1:
                     mark = "X";
                     break;
-                case(2):
+                case 2:
                     mark = "O";
                     break;
             }
-            /*
-            switch (intPlayerInput)
-            {
-                case(1):
-                    arrayTicTacToe[1,1] = mark;
-                    break;
-            }
-            */
-            //loop through array - failed maybe Denis will do it..
+
+            //run through array (need to do this 1000 times to remember it 
             for (var i = 0; i < arrayTicTacToe.GetLength(0); i++)
             {
                 for (var j = 0; j < arrayTicTacToe.GetLength(1); j++)
                 {
-                    if (arrayTicTacToe[i, j] == intPlayerInput.ToString())
+                    if (arrayTicTacToe[i, j] == intPlayerInput.ToString()
+                    ) // if inputted number is the same as "slot number" in array. :) 
                     {
                         arrayTicTacToe[i, j] = mark;
                     }
@@ -131,10 +134,77 @@ namespace CompleteCSharpMasterclass
 
         static void CheckWinStatus()
         {
-            _gameWon = false;
-            _gameWon = true;
+            //Horizontal Win
+            if (arrayTicTacToe[1, 1] == arrayTicTacToe[1, 5] && arrayTicTacToe[1, 5] == arrayTicTacToe[1, 9])
+            {
+                _gameWon = true;
+            }
+
+            if (arrayTicTacToe[4, 1] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[4, 9])
+            {
+                _gameWon = true;
+            }
+
+            if (arrayTicTacToe[8, 1] == arrayTicTacToe[8, 5] && arrayTicTacToe[8, 5] == arrayTicTacToe[8, 9])
+            {
+                _gameWon = true;
+            }
+
+            //Vertical Win
+            if (arrayTicTacToe[1, 1] == arrayTicTacToe[4, 1] && arrayTicTacToe[4, 1] == arrayTicTacToe[8, 1])
+            {
+                _gameWon = true;
+            }
+
+            if (arrayTicTacToe[1, 5] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[8, 5])
+            {
+                _gameWon = true;
+            }
+
+            if (arrayTicTacToe[1, 9] == arrayTicTacToe[4, 9] && arrayTicTacToe[4, 9] == arrayTicTacToe[8, 9])
+            {
+                _gameWon = true;
+            }
+
+            //Diagonal Win
+            if (arrayTicTacToe[1, 1] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[8, 9])
+            {
+                _gameWon = true;
+            }
+
+            if (arrayTicTacToe[1, 9] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[8, 1])
+
+            {
+                _gameWon = true;
+            }
+
+            if (AllFieldsFull())
+            {
+                _gameDraw = true;
+            }
+
+
+
+        }
+
+        static bool AllFieldsFull()
+        {
+            for (var i = 0; i < arrayTicTacToe.GetLength(0); i++)
+            {
+                for (var j = 0; j < arrayTicTacToe.GetLength(1); j++)
+                {
+                    if ((arrayTicTacToe[i, j].Contains("1")) || (arrayTicTacToe[i, j].Contains("2")) || (arrayTicTacToe[i, j].Contains("3")) || (arrayTicTacToe[i, j].Contains("4"))|| (arrayTicTacToe[i, j].Contains("5"))|| (arrayTicTacToe[i, j].Contains("6"))|| (arrayTicTacToe[i, j].Contains("7"))|| (arrayTicTacToe[i, j].Contains("8")) || (arrayTicTacToe[i, j].Contains("9")))
+                    {
+                        return  false;
+                    }
+
+                }
+            }
+
+            return true;
         }
     }
+
 }
 
 
