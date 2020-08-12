b1eb39af8b26189f92b181e18361f3a9d7c9687e
Jonescifer <wolf_paul@icloud.com>
2020-08-12 11:23:59 +0300

    TicTacToe wip - simplified main method.. made ShowGame and PlayGame methods..

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
 using System.Net;
 using System.Runtime.Remoting.Messaging;
@@ -7,51 +8,74 @@ namespace CompleteCSharpMasterclass
 {
     class Program
     {
+        static bool playerOneWin = false;
+        static bool playerTwoWin = false;
+        static bool gameWon = false;
+        static int playerNumber = 0;
+        static string[,] ticTacToe =
+                    {
+                        {" "," "," ","|"," "," "," ","|"," "," "," "},
+                        {" ","1"," ","|"," ","2"," ","|"," ","3"," "},
+                        {"_","_","_","|","_","_","_","|","_","_","_"},
+                        {" "," "," ","|"," "," "," ","|"," "," "," "},
+                        {" ","4"," ","|"," ","5"," ","|"," ","6"," "},
+                        {" "," "," ","|"," "," "," ","|"," "," "," "},
+                        {"_","_","_","|","_","_","_","|","_","_","_"},
+                        {" "," "," ","|"," "," "," ","|"," "," "," "},
+                        {" ","7"," ","|"," ","8"," ","|"," ","9"," "},
+                        {" "," "," ","|"," "," "," ","|"," "," "," "}
+                    };
+        
+        
         public static void Main(string[] args)
         {
-            string[,] ticTacToe =
-            {
-                {" "," "," ","|"," "," "," ","|"," "," "," "},
-                {" ","1"," ","|"," ","2"," ","|"," ","3"," "},
-                {"_","_","_","|","_","_","_","|","_","_","_"},
-                {" "," "," ","|"," "," "," ","|"," "," "," "},
-                {" ","4"," ","|"," ","5"," ","|"," ","6"," "},
-                {" "," "," ","|"," "," "," ","|"," "," "," "},
-                {"_","_","_","|","_","_","_","|","_","_","_"},
-                {" "," "," ","|"," "," "," ","|"," "," "," "},
-                {" ","7"," ","|"," ","8"," ","|"," ","9"," "},
-                {" "," "," ","|"," "," "," ","|"," "," "," "}
-            };
-            
             ShowGame(ticTacToe);
-            UserInput();
-            CheckGame(ticTacToe);
-
-
+            RunGame();
+            
         }
-
         static void ShowGame(string[,] ticTacToe)
         {
-            
-            
-            for (int i = 0; i < ticTacToe.GetLength(0); i++)
+            for (var i = 0; i < ticTacToe.GetLength(0); i++)
             { 
-                for (int j = 0; j < ticTacToe.GetLength(1); j++)  
+                for (var j = 0; j < ticTacToe.GetLength(1); j++)  
                 { 
                     Console.Write("{0}" , ticTacToe[i,j]);
                 } 
                 Console.WriteLine();
             }
         }
-
-        static int UserInput()
+        static void RunGame()
         {
-            string input = Console.ReadLine();
-            
-            //if is int 1-9 return intInput.
-            
-            return intInput;
+            while (!gameWon)
+            { 
+                CheckGame(ticTacToe); 
+                if (playerOneWin || playerTwoWin)
+                { 
+                    Console.WriteLine("Player {0} has won!" , playerNumber ); 
+                    break;
+                }
+            }
         }
+        
+        static void CheckGame(string [,] ticTacToe)
+        {
+            //ask user for input.
+            Console.WriteLine("Player 1: Choose your field!");
+            
+            // check if is a number and return it.
+            var isNumeric = int.TryParse(Console.ReadLine(), out int intPlayerInput);
+
+            if (isNumeric)
+            {
+                if (intPlayerInput != 0 && intPlayerInput <= 9)
+                {
+                    
+                }
+            }
+            //wrong input entered
+            Console.WriteLine("Please enter a valid number between 1 and 9!");
+        }
+
         
     }
 }
