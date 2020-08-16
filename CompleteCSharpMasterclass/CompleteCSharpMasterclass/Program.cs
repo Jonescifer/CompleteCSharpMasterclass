616ccdafb1b0b5212b44ecb1c1ee5384a32609b5
Jonescifer <wolf_paul@icloud.com>
2020-08-16 08:39:32 +0300

    tictactoe... done Denis

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -1,215 +1,187 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
+using System.Media;
 using System.Net;
 using System.Runtime.Remoting.Messaging;
 
 namespace CompleteCSharpMasterclass
 {
-    class Program
+    public class temp
     {
-        static bool _gameWon;
-        private static bool _gameDraw = false;
-        static int _currentPlayer = 1; // at the beginning the first player is 1
-
-        static string[,] arrayTicTacToe =
+        public static char[,] playField =
         {
-            //0    1    2    3    4    5    6    7    8    9  
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //0
-            {" ", "1", " ", "|", " ", "2", " ", "|", " ", "3"}, //1
-            {"_", "_", "_", "|", "_", "_", "_", "|", "_", "_"}, //2
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //3
-            {" ", "4", " ", "|", " ", "5", " ", "|", " ", "6"}, //4
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //5
-            {"_", "_", "_", "|", "_", "_", "_", "|", "_", "_"}, //6
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "}, //7
-            {" ", "7", " ", "|", " ", "8", " ", "|", " ", "9"}, //8
-            {" ", " ", " ", "|", " ", " ", " ", "|", " ", " "} //9
+            //0  //1  //2
+            {'1', '2', '3'},// 0
+            {'4', '5', '6'},// 1
+            {'7', '8', '9'} // 2
         };
-
-
+        
+        public static char[,] playFieldInitial =
+        {
+            //0  //1  //2
+            {'1', '2', '3'},// 0
+            {'4', '5', '6'},// 1
+            {'7', '8', '9'} // 2
+        };
+          
+        static int turns = 0;
+        
         public static void Main(string[] args)
         {
-            ShowGame(arrayTicTacToe);
-            RunGame();
+            int player = 2;
+            int input = 0;//no case for 0 in EnterXorO method so no worries its going to be changed at start of game!
+            bool inputCorrect = true;
+          
 
-        }
 
-        static void ShowGame(string[,] ticTacToe)
-        {
-            for (var i = 0; i < ticTacToe.GetLength(0); i++)
+            do //Run code as long as program runs... duh (or not! Duh)
             {
-                for (var j = 0; j < ticTacToe.GetLength(1); j++)
+
+                if (player == 2)
                 {
-                    Console.Write("{0}", ticTacToe[i, j]);
+                    player = 1;
+                    EnterXorO(player, input); //no case for 0 in EnterXorO method so no worries its going to be changed at start of game!
+                }
+                else if (player == 1)
+                {
+                    player = 2;
+                    EnterXorO(player, input); //no case for 0 in EnterXorO method so no worries its going to be changed at start of game!
                 }
 
-                Console.WriteLine();
-            }
-        }
+                SetField();
 
-        static void RunGame()
-        {
-            while (!_gameWon || !_gameDraw)
-            {
-                CheckGameStatus();
-                Console.Clear();
-                ShowGame(arrayTicTacToe);
-                if (_gameWon)
+                #region Check winning condition
+
+                char[] playerChars = {'X','O'};
+
+                foreach (char playerChar in playerChars)
                 {
-                    Console.WriteLine("Player {0} has won!", _currentPlayer);
-                    break;
-                }
-
-                if (_gameDraw)
-                {
-                    Console.WriteLine("Its a draw!");
-                    break;
-                }
-            }
-        }
-
-        static void CheckGameStatus()
-        {
-            //ask user for input.
-            Console.WriteLine("Player {0}: Choose your field!", _currentPlayer);
-
-            // check if is a number and return it.
-            var isNumeric = int.TryParse(Console.ReadLine(), out int intPlayerInput);
-
-            if (isNumeric && intPlayerInput > 0 && intPlayerInput <= 9)
-            {
-                switch (_currentPlayer)
-                {
-                    case 1:
-                        EditArray(intPlayerInput);
-                        CheckWinStatus();
-                        if (_gameWon)
-                        {
-                            break;
-                        }
-                        _currentPlayer = 2;
-                        break;
-                    case 2:
-                        EditArray(intPlayerInput);
-                        CheckWinStatus();
-                        if (_gameWon)
-                        {
-                            break;
-                        }
-                        _currentPlayer = 1;
-                        break;
-                }
-            }
-            else
-            {
-                //wrong input entered
-                Console.WriteLine("Please enter a valid number between 1 and 9!");
-                Console.ReadKey();
-            }
-
-        }
-
-        static void EditArray(int intPlayerInput)
-
-        {
-            string mark = "";
-
-            switch (_currentPlayer)
-            {
-                case 1:
-                    mark = "X";
-                    break;
-                case 2:
-                    mark = "O";
-                    break;
-            }
-
-            //run through array (need to do this 1000 times to remember it 
-            for (var i = 0; i < arrayTicTacToe.GetLength(0); i++)
-            {
-                for (var j = 0; j < arrayTicTacToe.GetLength(1); j++)
-                {
-                    if (arrayTicTacToe[i, j] == intPlayerInput.ToString()
-                    ) // if inputted number is the same as "slot number" in array. :) 
+                    if(((playField[0,0] == playerChar) && (playField[0,1] == playerChar) && (playField[0,2] == playerChar))
+                        || ((playField[1,0] == playerChar) && (playField[1,1] == playerChar) && (playField[1,2] == playerChar))
+                        || ((playField[2,0] == playerChar) && (playField[2,1] == playerChar) && (playField[2,2] == playerChar))
+                        || ((playField[0,0] == playerChar) && (playField[1,0] == playerChar) && (playField[2,0] == playerChar))
+                        || ((playField[0,1] == playerChar) && (playField[1,1] == playerChar) && (playField[2,1] == playerChar))
+                        || ((playField[0,2] == playerChar) && (playField[1,2] == playerChar) && (playField[2,2] == playerChar))
+                        || ((playField[0,0] == playerChar) && (playField[1,1] == playerChar) && (playField[2,2] == playerChar))
+                        || ((playField[2,0] == playerChar) && (playField[1,1] == playerChar) && (playField[0,2] == playerChar))
+                        )
                     {
-                        arrayTicTacToe[i, j] = mark;
+                        if (playerChar == 'X')
+                        {
+                            Console.WriteLine("\nplayer 2 has won!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nplayer 1 has won!");
+                        }
+
+                        Console.WriteLine("press any key to reset the game!");
+                        Console.ReadKey();
+                        ResetField();
+                        player = 1;//to reset to player 1.... duh
+                        break;
+                    }
+                    else if (turns==10)
+                    {
+                        Console.WriteLine("WE HAVE A DRAW! \npress any key to reset the game!");
+                        Console.ReadKey();
+                          ResetField();
+                          player = 1;
+                          break;
                     }
                 }
+                
 
-            }
-        }
+                #endregion
+                #region Check user input and if filed is already taken...
 
-        static void CheckWinStatus()
-        {
-            //Horizontal Win
-            if (arrayTicTacToe[1, 1] == arrayTicTacToe[1, 5] && arrayTicTacToe[1, 5] == arrayTicTacToe[1, 9])
-            {
-                _gameWon = true;
-            }
-
-            if (arrayTicTacToe[4, 1] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[4, 9])
-            {
-                _gameWon = true;
-            }
-
-            if (arrayTicTacToe[8, 1] == arrayTicTacToe[8, 5] && arrayTicTacToe[8, 5] == arrayTicTacToe[8, 9])
-            {
-                _gameWon = true;
-            }
-
-            //Vertical Win
-            if (arrayTicTacToe[1, 1] == arrayTicTacToe[4, 1] && arrayTicTacToe[4, 1] == arrayTicTacToe[8, 1])
-            {
-                _gameWon = true;
-            }
-
-            if (arrayTicTacToe[1, 5] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[8, 5])
-            {
-                _gameWon = true;
-            }
-
-            if (arrayTicTacToe[1, 9] == arrayTicTacToe[4, 9] && arrayTicTacToe[4, 9] == arrayTicTacToe[8, 9])
-            {
-                _gameWon = true;
-            }
-
-            //Diagonal Win
-            if (arrayTicTacToe[1, 1] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[8, 9])
-            {
-                _gameWon = true;
-            }
-
-            if (arrayTicTacToe[1, 9] == arrayTicTacToe[4, 5] && arrayTicTacToe[4, 5] == arrayTicTacToe[8, 1])
-
-            {
-                _gameWon = true;
-            }
-
-            if (AllFieldsFull())
-            {
-                _gameDraw = true;
-            }
-        }
-
-        static bool AllFieldsFull()
-        {
-            for (var i = 0; i < arrayTicTacToe.GetLength(0); i++)
-            {
-                for (var j = 0; j < arrayTicTacToe.GetLength(1); j++)
+                do
                 {
-                    if ((arrayTicTacToe[i, j].Contains("1")) || (arrayTicTacToe[i, j].Contains("2")) || (arrayTicTacToe[i, j].Contains("3")) || (arrayTicTacToe[i, j].Contains("4"))|| (arrayTicTacToe[i, j].Contains("5"))|| (arrayTicTacToe[i, j].Contains("6"))|| (arrayTicTacToe[i, j].Contains("7"))|| (arrayTicTacToe[i, j].Contains("8")) || (arrayTicTacToe[i, j].Contains("9")))
+                    Console.Write("\nPlayer {0}: Choose your field!", player);
+                    try
                     {
-                        return  false;
+                        input = Convert.ToInt16(Console.ReadLine()); // i uset try parse and throw message if it wasnt a number.. couse this is how i roll..
                     }
+                    catch
+                    {
+                        Console.WriteLine("please enter a number");
+                        Console.ReadKey();
+                    }
+                    if ((input == 1) && (playField[0,0]=='1'))
+                        inputCorrect = true;
+                    else if ((input == 2) && (playField[0,1]=='2'))
+                        inputCorrect = true;
+                    else if ((input == 3) && (playField[0,2]=='3'))
+                        inputCorrect = true;
+                    else if ((input == 4) && (playField[1,0]=='4'))
+                        inputCorrect = true;
+                    else if ((input == 5) && (playField[1,1]=='5'))
+                        inputCorrect = true;
+                    else if ((input == 6) && (playField[1,2]=='6'))
+                        inputCorrect = true;
+                    else if ((input == 7) && (playField[2,0]=='7'))
+                        inputCorrect = true;
+                    else if ((input == 8) && (playField[2,1]=='8'))
+                        inputCorrect = true;
+                    else if ((input == 9) && (playField[2,2]=='9'))
+                        inputCorrect = true;
+                    else
+                    {
+                        Console.WriteLine("field taken, use another field.");
+                        inputCorrect = false;
+                    }
+                    
 
-                }
+                } while (!inputCorrect);
+                #endregion
+            } while (true);
+        }
+
+        public static void SetField()
+        {
+            Console.Clear();
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  " , playField[0,0],playField[0,1],playField[0,2]);
+            Console.WriteLine("-----|-----|-----");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  " , playField[1,0],playField[1,1],playField[1,2]);
+            Console.WriteLine("-----|-----|-----");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  " , playField[2,0],playField[2,1],playField[2,2]);
+            Console.WriteLine("     |     |     ");
+            turns++; // counts turns!
+        }
+
+        public static void ResetField()
+        {
+            playField = playFieldInitial;
+            SetField();
+            turns = 0;
+        }
+        
+
+        public static void EnterXorO(int player , int input)
+        {
+            char playerMark=' ';// need to check player 1 is O..
+            if (player==1)
+            {
+                playerMark = 'X';
+            }else if(player==2)
+
+            {
+                playerMark = 'O';
             }
-
-            return true;
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerMark; break;
+                case 2: playField[0, 1] = playerMark; break;
+                case 3: playField[0, 2] = playerMark; break;
+                case 4: playField[1, 0] = playerMark; break;
+                case 5: playField[1, 1] = playerMark; break;
+                case 6: playField[1, 2] = playerMark; break;
+                case 7: playField[2, 0] = playerMark; break;
+                case 8: playField[2, 1] = playerMark; break;
+                case 9: playField[2, 2] = playerMark; break;
+            }
         }
     }
-
 }
-
-
