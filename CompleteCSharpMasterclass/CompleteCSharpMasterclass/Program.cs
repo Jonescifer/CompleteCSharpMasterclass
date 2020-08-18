e4ca9fdaeec5d8549bb15a92b1f55fa0c2d8519c
Jonescifer <wolf_paul@icloud.com>
2020-08-18 08:53:12 +0300

    jagged array finished

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -7,181 +7,60 @@ using System.Runtime.Remoting.Messaging;
 
 namespace CompleteCSharpMasterclass
 {
-    public class temp
+    public class MainClass
     {
-        public static char[,] playField =
-        {
-            //0  //1  //2
-            {'1', '2', '3'},// 0
-            {'4', '5', '6'},// 1
-            {'7', '8', '9'} // 2
-        };
-        
-        public static char[,] playFieldInitial =
-        {
-            //0  //1  //2
-            {'1', '2', '3'},// 0
-            {'4', '5', '6'},// 1
-            {'7', '8', '9'} // 2
-        };
-          
-        static int turns = 0;
-        
         public static void Main(string[] args)
         {
-            int player = 2;
-            int input = 0;//no case for 0 in EnterXorO method so no worries its going to be changed at start of game!
-            bool inputCorrect = true;
-          
+            /*
+            // declare jagged array
+            int [][] jaggedArray = new int[3][]; 
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int [3];
+            jaggedArray[2] = new int[2];
 
-
-            do //Run code as long as program runs... duh (or not! Duh)
+            jaggedArray[0] = new[] {2, 3, 5, 7, 11};
+            jaggedArray[1] = new[] {1, 2, 3};
+            jaggedArray[2] = new[] {13,21};
+            
+            //alternative way to declare jagged array.
+            int[][] jaggedArray2 = new int [][]
             {
+                new int[] {2, 3, 5, 7, 11},
+                new int[] {1, 2, 3},
+                new int[] {13,21}
+            };
 
-                if (player == 2)
+            Console.WriteLine("the value in array 0 third index is {0}", jaggedArray2[0][2]);
+
+            for (int i =0 ; i < jaggedArray2.Length; i++)
+            {
+                Console.WriteLine("Element {0}", i);
+
+                for (int j = 0 ; j < jaggedArray2[i].Length; j++)
                 {
-                    player = 1;
-                    EnterXorO(player, input); //no case for 0 in EnterXorO method so no worries its going to be changed at start of game!
-                }
-                else if (player == 1)
-                {
-                    player = 2;
-                    EnterXorO(player, input); //no case for 0 in EnterXorO method so no worries its going to be changed at start of game!
-                }
-
-                SetField();
-
-                #region Check winning condition
-
-                char[] playerChars = {'X','O'};
-
-                foreach (char playerChar in playerChars)
-                {
-                    if(((playField[0,0] == playerChar) && (playField[0,1] == playerChar) && (playField[0,2] == playerChar))
-                        || ((playField[1,0] == playerChar) && (playField[1,1] == playerChar) && (playField[1,2] == playerChar))
-                        || ((playField[2,0] == playerChar) && (playField[2,1] == playerChar) && (playField[2,2] == playerChar))
-                        || ((playField[0,0] == playerChar) && (playField[1,0] == playerChar) && (playField[2,0] == playerChar))
-                        || ((playField[0,1] == playerChar) && (playField[1,1] == playerChar) && (playField[2,1] == playerChar))
-                        || ((playField[0,2] == playerChar) && (playField[1,2] == playerChar) && (playField[2,2] == playerChar))
-                        || ((playField[0,0] == playerChar) && (playField[1,1] == playerChar) && (playField[2,2] == playerChar))
-                        || ((playField[2,0] == playerChar) && (playField[1,1] == playerChar) && (playField[0,2] == playerChar))
-                        )
-                    {
-                        if (playerChar == 'X')
-                        {
-                            Console.WriteLine("\nplayer 2 has won!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\nplayer 1 has won!");
-                        }
-
-                        Console.WriteLine("press any key to reset the game!");
-                        Console.ReadKey();
-                        ResetField();
-                        player = 1;//to reset to player 1.... duh
-                        break;
-                    }
-                    else if (turns==10)
-                    {
-                        Console.WriteLine("WE HAVE A DRAW! \npress any key to reset the game!");
-                        Console.ReadKey();
-                          ResetField();
-                          player = 1;
-                          break;
-                    }
+                    Console.WriteLine("{0} ", j);
                 }
                 
-
-                #endregion
-                #region Check user input and if filed is already taken...
-
-                do
-                {
-                    Console.Write("\nPlayer {0}: Choose your field!", player);
-                    try
-                    {
-                        input = Convert.ToInt16(Console.ReadLine()); // i uset try parse and throw message if it wasnt a number.. couse this is how i roll..
-                    }
-                    catch
-                    {
-                        Console.WriteLine("please enter a number");
-                        Console.ReadKey();
-                    }
-                    if ((input == 1) && (playField[0,0]=='1'))
-                        inputCorrect = true;
-                    else if ((input == 2) && (playField[0,1]=='2'))
-                        inputCorrect = true;
-                    else if ((input == 3) && (playField[0,2]=='3'))
-                        inputCorrect = true;
-                    else if ((input == 4) && (playField[1,0]=='4'))
-                        inputCorrect = true;
-                    else if ((input == 5) && (playField[1,1]=='5'))
-                        inputCorrect = true;
-                    else if ((input == 6) && (playField[1,2]=='6'))
-                        inputCorrect = true;
-                    else if ((input == 7) && (playField[2,0]=='7'))
-                        inputCorrect = true;
-                    else if ((input == 8) && (playField[2,1]=='8'))
-                        inputCorrect = true;
-                    else if ((input == 9) && (playField[2,2]=='9'))
-                        inputCorrect = true;
-                    else
-                    {
-                        Console.WriteLine("field taken, use another field.");
-                        inputCorrect = false;
-                    }
-                    
-
-                } while (!inputCorrect);
-                #endregion
-            } while (true);
-        }
-
-        public static void SetField()
-        {
-            Console.Clear();
-            Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  " , playField[0,0],playField[0,1],playField[0,2]);
-            Console.WriteLine("-----|-----|-----");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  " , playField[1,0],playField[1,1],playField[1,2]);
-            Console.WriteLine("-----|-----|-----");
-            Console.WriteLine("  {0}  |  {1}  |  {2}  " , playField[2,0],playField[2,1],playField[2,2]);
-            Console.WriteLine("     |     |     ");
-            turns++; // counts turns!
-        }
-
-        public static void ResetField()
-        {
-            playField = playFieldInitial;
-            SetField();
-            turns = 0;
-        }
-        
-
-        public static void EnterXorO(int player , int input)
-        {
-            char playerMark=' ';// need to check player 1 is O..
-            if (player==1)
-            {
-                playerMark = 'X';
-            }else if(player==2)
-
-            {
-                playerMark = 'O';
             }
-            switch (input)
+
+            Console.WriteLine("Done counting");
+           */
+
+            string[][] friendsAndFamily = new string[][]
             {
-                case 1: playField[0, 0] = playerMark; break;
-                case 2: playField[0, 1] = playerMark; break;
-                case 3: playField[0, 2] = playerMark; break;
-                case 4: playField[1, 0] = playerMark; break;
-                case 5: playField[1, 1] = playerMark; break;
-                case 6: playField[1, 2] = playerMark; break;
-                case 7: playField[2, 0] = playerMark; break;
-                case 8: playField[2, 1] = playerMark; break;
-                case 9: playField[2, 2] = playerMark; break;
-            }
+                new string[] {"Maria", "Liliana"},
+                new string[] {"Igal", "Ran"},
+                new string[] {"David", "Joe"}
+            };
+            Console.WriteLine("Hi, {0}, i would like to introduce {1} to you...", friendsAndFamily[0][0] , friendsAndFamily[1][1]);
+            Console.WriteLine("Hi, {0}, i would like to introduce {1} to you...", friendsAndFamily[0][1] , friendsAndFamily[1][0]);
+            Console.WriteLine("Hi, {0}, i would like to introduce {1} to you...", friendsAndFamily[2][1] , friendsAndFamily[0][1]);
         }
+
     }
 }
+
+
+
+    
+
