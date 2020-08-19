c66b34545e95d4c39cf1b88afd9b3bde53b1cbc6
Jonescifer <wolf_paul@icloud.com>
2020-08-19 11:20:50 +0300

    enlightment about arrays as arguments!

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -12,7 +12,7 @@ namespace CompleteCSharpMasterclass
         static void Main(string[] args)
         {
             int[] happinessArray = new[] {2, 3, 4, 5, 6};
-            
+
             SunIsShining(happinessArray);
 
             int index = 0;// wanted to have index. for eace loops dont have index.. so i made an external variable and used it as the index of the array.
@@ -25,6 +25,14 @@ namespace CompleteCSharpMasterclass
             }
         }
 
+        /*
+         TURNS OUT I DIDNT UNDERSTAND THIS!!!! I DIDNT UNDERSTAND HOW TO CHANGE THE happinessARRAY, i thought i only changed the x array inside the method. BUT NO! an array is a reference type. so you make changes to the actual argument( happiness array)!!!
+         
+         Why you can change an array by passing it as a variable
+         If you tried to run the Method call SunIsShining(happiness);  without assigning it to happiness you might have realized, that happiness itself is changed. Why is that?
+         Arrays are a reference type in C#. This means that each time an array is passed as an argument to any function, the reference (or pointer) to the argument is passed to the function.
+         Thus any modifications you make to the array in the function are made in the actual argument also
+        */
         static void SunIsShining(int[] x) // it will also assign these new values to the happiness array!!!! 
         {
             for(int i = 0 ; i < x.Length ; i++)
