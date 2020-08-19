28481cf0dad56765a363a6ad42cdd4d0edafae92
Jonescifer <wolf_paul@icloud.com>
2020-08-19 16:20:43 +0300

    partyFriends

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -13,72 +13,43 @@ namespace CompleteCSharpMasterclass
     {
         static void Main(string[] args)
         {
-           //int ARRAY immutable(fixed array size) & limited to one type
-           int[] array = new int[]{99,96,87,76};
-           
-           //____________________________________________________________________________________________________________________________________________________________________
-           
-           //List Object mutable!
-           var list = new List <int>{1,2,3,4};
-           
-           list.Add(0); list.Add(32); 
-           list.Sort();//sorts the list by default comparer
-               
-            foreach (var element in list) 
-            { 
-                Console.WriteLine(element);
-            }
+            var friends = new List<string>{"Frank", "Joe","Michelle" , "Andy", "Maria", "Carlos", "Angelina"};
+            var partyFriend = GetPartyFriends(friends, 3);
 
-            list.RemoveRange(2,2);
-
-            foreach (var element in list) 
-            { 
-                Console.WriteLine(element);
-            }
-            
-            Console.WriteLine("is value 32 in the list? {0}", list.Contains(32));// does it contain element with value 32
-            
-            int index = list.FindIndex(x => x == 4); // find the index of first element that has 4.
-            Console.WriteLine("index of first element that contains the value 4 is: {0}" , index);        // index of the first occurence of value 4. is index 2. third place in the list..
-            Console.WriteLine("value at index {0} is {1}", index, list[index]); // value at the potion of index... value is 4 index is 2
-            list.RemoveAt(index);
-            list.ForEach(i => Console.WriteLine(i)); // shorter forEach loop which is a method of list!!!!!!! using lambda expression
-            
-            Console.WriteLine();
-
-            var objectList = new List<object>(); // can be a list of objects!
-            
-            //____________________________________________________________________________________________________________________________________________________________________
-            
-            //ArrayList can also store objects and any datatype together.
-            var arrayList = new ArrayList();
-            arrayList.Add(1);
-            arrayList.Add("Two");
-            arrayList.Add("3");
-            arrayList.Add(new Number{N = 4} );// added new object and set the N variable to 4.
-
-            foreach (var o in arrayList)
+            foreach (var name in partyFriend)
             {
-                Console.WriteLine(o);
+                Console.WriteLine(name);
             }
-
-
-
-
-
-
+            
         }
 
-    }
-    //need to create this specific class just so i can show the number 4 in the Number object.. need to create the PROPERTY to assign the value to object Number then need to 
-    //create the METHOD override string to show the number as string...
-    class Number// for the example of adding an object to arrayList or List
-    {
-        public int N { get; set; }
-
-        public override string ToString()
+        public static List<string> GetPartyFriends(List<string> list, int count)
         {
-            return N.ToString();
+            var partyFriends = new List<string>();
+            
+            while (partyFriends.Count < count)
+            {
+                var currentFriend = GetPartyFriend(list);
+                
+                partyFriends.Add(currentFriend);
+                list.Remove(currentFriend);
+            }
+            
+            return partyFriends;
+        }
+
+        public static string GetPartyFriend(List<string> list)
+        {
+            string shortestName = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length > shortestName.Length)
+                {
+                    shortestName = list[i];
+                }
+            }
+
+            return shortestName;
         }
     }
 }
