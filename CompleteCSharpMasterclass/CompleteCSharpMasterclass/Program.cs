50b2b1f815e17dee31fa87aa7246a696e5b429f8
Jonescifer <wolf_paul@icloud.com>
2020-09-09 17:53:30 +0300

    enum

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -7,35 +7,46 @@ namespace CompleteCSharpMasterclass
 {
     class MainClass
     {
-        public struct Game
+        // Enums a re there to share a set of constants to keep the library consistent.
+        private enum Day
         {
-            public string name;
-            public string developer;
-            public double rating;
-            public string releaseDate;
+            Su,
+            Mo,
+            Tu,
+            We,
+            Th,
+            Fr,
+            Sa
+        };
 
-           //doesnt have a default constructor - i.e. empty.... null...
-           //public Game(){}
-            public Game(string name, string developer,double rating, string releaseDate)
-            { 
-                this.name = name;
-                this.developer = developer;
-                this.rating = rating;
-                this.releaseDate = releaseDate;
-            }
-            
-            public void DisplayInfo()
-            { 
-                Console.WriteLine($"The game {name} was released by {developer}, on {releaseDate} and it's rating is {rating}."); 
-            }
-        }
+        private enum Month
+        {
+            Jan=1,
+            Feb,
+            Mar,
+            Apr,
+            May,
+            Jun,
+            Jul=12,
+            Aug,
+            Sep,
+            Oct,
+            Nov,
+            Dec
+        };
         
         private static void Main(string[] args)
         {
-            //have to assign all the variables before using it's methods!
-            var game1 = new Game("Pokemon Go","Niantic",3.5,"01.07.2016");
-            game1.DisplayInfo();
-            
+            var friday = Day.Fr;
+            var sunday = Day.Su;
+            var dayA = Day.Fr;
+
+            Console.WriteLine(friday==dayA);
+            Console.WriteLine(Day.Mo);
+            Console.WriteLine((int)Day.Su);
+
+            Console.WriteLine((int)Month.Feb);
+            Console.WriteLine((int)Month.Aug);
         }
     }
 }
