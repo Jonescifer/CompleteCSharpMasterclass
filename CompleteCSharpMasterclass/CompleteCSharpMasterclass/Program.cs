abd2b82f01a1c627880e91fca959021a88f0561b
Jonescifer <wolf_paul@icloud.com>
2020-09-13 09:31:34 +0300

    regex part 1

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -10,35 +10,6 @@ namespace CompleteCSharpMasterclass
         private static void Main(string[] args)
         {
             
-            // //random dice face
-            // var randomDice = new Random();
-            // int numEyes;
-            // for (var i = 0; i < 10; i++)
-            // {
-            //     numEyes = randomDice.Next(1, 7);
-            //     Console.WriteLine(numEyes);
-            // }
-            //
-
-            //fortune teller
-            Console.WriteLine("Ask me a question..");
-            Console.ReadLine();
-            
-            var randomAnswer = new Random();
-            var answeNum = randomAnswer.Next(1, 4);
-
-            switch (answeNum)
-            {
-                case 1:
-                    Console.WriteLine("yes");
-                    return;
-                case 2:
-                    Console.WriteLine("maybe");
-                    return;
-                case 3:
-                    Console.WriteLine("no");
-                    return;
-            }
         }
     }
 }
