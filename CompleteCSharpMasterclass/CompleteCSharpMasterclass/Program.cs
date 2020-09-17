4683a8adbe02c40b0300c47286adfc644a8a5079
Paul Wolf <paul@inceptionxr.com>
2020-09-17 13:44:09 +0300

    Regex find patterns in a string..

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -2,6 +2,7 @@
 using System.CodeDom;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace CompleteCSharpMasterclass
 {
@@ -9,6 +10,20 @@ namespace CompleteCSharpMasterclass
     {
         private static void Main(string[] args)
         {
+            //the pattern to find: /d -> numbers
+            string patternToFind = @"\d";//@"\d{10} //@"number"
+            string text = "Hi my number is 0502226940";
+            var rx = new Regex(patternToFind);
+
+            MatchCollection matchCollection = rx.Matches(text);
+            
+            Console.WriteLine($"{matchCollection.Count} hits where found in: {text}.");
+
+            foreach (Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine($"{group[0].Value} appeared at index number: {group[0].Index}");
+            }
             
         }
     }
