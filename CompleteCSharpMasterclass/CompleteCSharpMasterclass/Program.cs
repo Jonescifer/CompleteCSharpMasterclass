ad6b069cbfc9bfa085bc5b6a372597c700231e7f
Paul Wolf <paul@inceptionxr.com>
2020-09-17 17:32:14 +0300

    nullables

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -8,93 +8,56 @@ namespace CompleteCSharpMasterclass
     {
         private static void Main(string[] args)
         {
-            var dateTime = new DateTime(1979, 06, 27, 08, 00, 00);
+            int? num1 = null;
+            //int num2 = null; cant work.
+            int? num2 = 1337;
+            double? num3 = new double?();
+            double? num4 = 3.14157;
+            bool? boolVal = new bool?();
 
-            Console.WriteLine($"My birthday is on: {dateTime}.");
+            Console.WriteLine($"Nullable numbers are:{num1},{num2},{num3},{num4}.");//the null is no value!
+            Console.WriteLine($"The nullable bool is:{boolVal}.");
 
-            // write today on screen
-            Console.WriteLine(DateTime.Today);
-            
-            //write today AND current LOCAL time on screen
-            Console.WriteLine(DateTime.Now);
-            
-            //write today AND current UTC time on screen
-            Console.WriteLine(DateTime.UtcNow);
-            
-            //get the day of tomorrow..
-            DateTime tomorrow = GetTomorrow();
-            Console.WriteLine($"tomorrow is: {tomorrow}");
-            
-            //get day of week today..
-            Console.WriteLine(DateTime.Now.DayOfWeek);
-            
-            //get first day of year..
-            var year = 1979;
-            Console.WriteLine($"The first day of {year} is: {GetFirstDayOfYear(year)}");
-            Console.WriteLine(DateTime.DaysInMonth(year,1));
-            
-            //get number of days in a specific month in a specific year.
-            var days = DateTime.DaysInMonth(1978,6);
-            Console.WriteLine($"june of 1978 has {days} days.");
-            
-            //get todays date time in a specific manner
-            DateTime now = DateTime.Now; //.AddDays(11)
-            Console.WriteLine($"minute {now.Minute}");
-            
-            
-            
-            //display the time in this structure x oclock y minutes and z seconds
-
-            Console.WriteLine($"the time is: {now.Hour} hours, {now.Minute} minutes and {now.Second}.");
-
-            Console.WriteLine("write a date in this format: yyy-mm-dd");
-            string input = Console.ReadLine();
-            
-            if (DateTime.TryParse(input, out DateTime dateTimeUser))
+            bool? isMale = null;
+            if (isMale == true)
             {
-                Console.WriteLine(dateTimeUser);
-                TimeSpan daysPassed = now.Subtract(dateTimeUser);
-                Console.WriteLine($"{daysPassed.Days} days have passes since {dateTimeUser}");
+                Console.WriteLine("user is male");
+            }
+            else if (isMale == false)
+            {
+                Console.WriteLine("user is feale");
             }
             else
             {
-                throw new Exception("wrong input mate!");
+                Console.WriteLine("User didnt define gender");
             }
+
+            double? num6 = 13.1;
+            double? num7 = null;
+            double num8;
+            //assigning a number to num8 if num6 is null or not - the long way......
+            if (num6 == null)
+            {
+                num8 = 0.0;
+            }
+            else
+            {
+                num8 = (double)num6;// cannot implicitly convert type 'double?' to 'double'. An explicit conversion  exists (are you missing a cast?)
+            }
+            Console.WriteLine($"value of num8 is {num8}");
             
-            Console.WriteLine($"enter a number of days you want to know back from today as a date");
             
-            //58 days ago i was 15000 days old. on 21.07.2020
-            int intInput1 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine(GetDateNDaysAgo(intInput1, now));
             
-            Console.WriteLine($"enter a number of days you want to know from today as a date");
+            //shorter version with
+            //'Null Coalescing Operator'
             
-            //days to go in the future
-            int intInput2 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine(GetDateNDaysTogo(intInput2, now));
-        }
-        //get tomorrow
-        static DateTime GetTomorrow()
-        {
-            return DateTime.Now.AddDays(1);
-        }
-        //get first day of the year
-        static DateTime GetFirstDayOfYear(int year)
-        {
-            return new DateTime(year,1 ,1 );
-        }
-        //what date was N days ago!
-        static string GetDateNDaysAgo(int days , DateTime now)
-        {
-            DateTime dateNDaysAgo = now.AddDays(-(days));
+            //num6 is not 'null' - so num 8 wont get this new value.
+            num8 = num6 ?? 8.3;
+            Console.WriteLine($"value of num8 is {num8}");
             
-            return dateNDaysAgo.ToString(CultureInfo.CurrentCulture);
-        }
-        static string GetDateNDaysTogo(int days , DateTime now)
-        {
-            DateTime dateNDaysAgo = now.AddDays(days);
-            
-            return dateNDaysAgo.ToString(CultureInfo.CurrentCulture);
+            //num7 is 'null' - so num8 will get 8.3
+            num8 = num7 ?? 8.3;
+            Console.WriteLine($"value of num8 is {num8}");
         }
     }
 }
