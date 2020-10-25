e3ef8ff8267571f2f8106b3f7e3da5a0e81f21f6
Paul Wolf <paul@inceptionxr.com>
2020-10-25 17:57:16 +0200

    threadpool

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace CompleteCSharpMasterclass
 {
@@ -14,39 +9,32 @@ namespace CompleteCSharpMasterclass
 
         private static void Main(string[] args)
         {
-            /*
-            new Thread (() =>
+            new Thread(() =>
             {
                 Thread.Sleep(1000);
-                Console.WriteLine("Thread 1");
-            }).Start();
-            new Thread (() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 2");
-            }).Start();
-            new Thread (() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 3");
-            }).Start();
-            new Thread (() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 4");
-            }).Start();
-            */
+                Console.WriteLine("Thread X");
+            }){IsBackground = true}.Start();
             
+            Console.WriteLine("Started");
             Enumerable.Range (0,1000).ToList().ForEach(f =>
-            { 
-                new Thread(() =>
+            {
+                Console.WriteLine($"loop: {f}");
+                ThreadPool.QueueUserWorkItem((o) =>
                 {
-                    Console.WriteLine($"thread number{Thread.CurrentThread.ManagedThreadId} started");
-                    Thread.Sleep(1000);
-                    Console.WriteLine($"thread number{Thread.CurrentThread.ManagedThreadId} ended");
-                }).Start();
+                    try
+                    {
+                        Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+                        //Thread.Sleep(1000);
+                        Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                });
             });
-            
+            Console.WriteLine("Ended");
         }
     }
 }
