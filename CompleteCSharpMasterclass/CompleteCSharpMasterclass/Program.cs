51e276461b2ef1ada1ca89eec992f3980f5c2a93
Jonescifer <wolf_paul@icloud.com>
2020-11-18 15:50:29 +0200

    thread Join  part 2/2

--- CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
+++ CompleteCSharpMasterclass/CompleteCSharpMasterclass/Program.cs
@@ -18,11 +18,27 @@ namespace CompleteCSharpMasterclass
             thread1.Start();
             thread2.Start(); 
             //block Main thread from stopping while this thread runs!!!!!(Blocks the calling thread until the thread represented by this instance terminates.)
-            thread1.Join();
-            Console.WriteLine("Thread1Function ended");
+            if (thread1.Join(1000))
+            {
+                Console.WriteLine("Thread1Function ended");
+            }
+            else
+            {
+                Console.WriteLine("Thread1Function wasn't done within 1 second");
+            }
+            
             //block Main thread from stopping while this thread runs!!!!!
             thread2.Join();
             Console.WriteLine("Thread2Function ended");
+
+            if (thread1.IsAlive)
+            {
+                Console.WriteLine("thread1 is still doing stuff");
+            }
+            else
+            {
+                Console.WriteLine("thread1 ended");
+            }
             
             Console.WriteLine("Main Thread ended"); //Main threads ends.. no more line of code..
         }
@@ -31,6 +47,7 @@ namespace CompleteCSharpMasterclass
         {
             Console.WriteLine("Thread1Function started");
             Thread.Sleep(3000);
+            Console.WriteLine("Thread1Function coming back to caller (Main)"); // this line will be executed after Main Class finishes running and Main thread ends. 
         }
         public static void Thread2Function()
         {
