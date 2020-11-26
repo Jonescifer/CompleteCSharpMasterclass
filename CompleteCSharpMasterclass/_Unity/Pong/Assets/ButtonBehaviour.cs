d5796a74056f42958becd4a53e3b3284e99ee738
Jonescifer <wolf_paul@icloud.com>
2020-11-26 17:15:50 +0200

    main to level 1 and back

---
+++ CompleteCSharpMasterclass/_Unity/Pong/Assets/ButtonBehaviour.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public void OnButtonPressed()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
---
+++ CompleteCSharpMasterclass/_Unity/Pong/Assets/ButtonBehaviour_Level1.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour_Level1 : MonoBehaviour
+{
+    public void OnButtonPressed()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
