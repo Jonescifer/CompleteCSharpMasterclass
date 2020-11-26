08d4bd8b8367c8297ef9f09ba21516eb92ce6f76
Jonescifer <wolf_paul@icloud.com>
2020-11-26 17:00:10 +0200

    text and button pressed example

---
+++ CompleteCSharpMasterclass/_Unity/Pong/Assets/Script/ButtonBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public Text scoreText;
+
+    public void OnButtonPressed()
+    {
+        scoreText.text = "Button was pressed";
+    }
+}
---
+++ CompleteCSharpMasterclass/_Unity/Pong/Assets/Script/TextBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextBehaviour : MonoBehaviour
+{
+    public Text myText;
+    private int _textNumber = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _textNumber++;
+            myText.text = _textNumber.ToString();
+
+        }else if (Input.GetKeyDown(KeyCode.S))
+        {
+            myText.text = "S was pressed";
+        }
+    }
+}
