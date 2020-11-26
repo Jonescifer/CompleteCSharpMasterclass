898995858a4591ae6294ec3845381bed85483277
Jonescifer <wolf_paul@icloud.com>
2020-11-26 14:54:20 +0200

    Done Intro

--- CompleteCSharpMasterclass/_Unity/Unity Basics/Assets/Scripts/NewBehaviourScript.cs
+++ CompleteCSharpMasterclass/_Unity/Unity Basics/Assets/Scripts/NewBehaviourScript.cs
@@ -7,12 +7,16 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Start Method was called");
+        float myRandomNumber = Random.Range(0f, 15f);
+        print($"Start random number: {myRandomNumber}");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("Update Method was called");
+        float myRandomNumber = Random.Range(0f, 15f);
+        print($"Updated random number: {myRandomNumber}");
     }
 }
