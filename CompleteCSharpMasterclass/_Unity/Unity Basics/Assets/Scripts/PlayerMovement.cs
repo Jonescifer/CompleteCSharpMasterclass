42e1f9a293e6f15c00d89385b74e29f39285aa79
Jonescifer <wolf_paul@icloud.com>
2020-11-26 13:31:12 +0200

    unity wip

---
+++ CompleteCSharpMasterclass/_Unity/Unity Basics/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public bool paulHaMelech = true;
+
+    private Rigidbody _myRigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementVertical = Input.GetAxis("Vertical");
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        
+        Vector3 movement = new Vector3(movementHorizontal, 0f,movementVertical);
+        
+        _myRigidbody.AddForce(movement * speed);
+        
+    }
+}
