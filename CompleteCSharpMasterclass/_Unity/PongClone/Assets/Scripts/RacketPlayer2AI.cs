d2a9aa4521c5cb1f6124574224221ce0089b35d4
Jonescifer <wolf_paul@icloud.com>
2020-12-09 13:34:42 +0200

    done Denis's Pong - Added AI.

---
+++ CompleteCSharpMasterclass/_Unity/PongClone/Assets/Scripts/RacketPlayer2AI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class RacketPlayer2AI : MonoBehaviour
+{
+    public float movementSpeed = 200;
+    public GameObject ball;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(this.transform.position.y - ball.transform.position.y) > 50)
+        {
+            if (this.transform.position.y > ball.transform.position.y)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0,-1)* movementSpeed;
+            }
+            else if(this.transform.position.y < ball.transform.position.y)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0,1)* movementSpeed; 
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0,0); 
+            }
+        }
+    }
+}
+
