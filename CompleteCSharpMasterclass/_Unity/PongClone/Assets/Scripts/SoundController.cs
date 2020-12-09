ee40377830c89f6a0c5008cf91ca3f6fc6f1a3b2
Jonescifer <wolf_paul@icloud.com>
2020-12-09 13:00:10 +0200

    added sound

---
+++ CompleteCSharpMasterclass/_Unity/PongClone/Assets/Scripts/SoundController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource _wallSound;
+    public AudioSource _racketSound;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "WallTop" || other.gameObject.name == "WallBottom")
+        {
+            _wallSound.Play();
+        }else if (other.gameObject.name == "RacketPlayer1" || other.gameObject.name == "RacketPlayer2")
+        {
+            _racketSound.Play();
+        }
+    }
+}
