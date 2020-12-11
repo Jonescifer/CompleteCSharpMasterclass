c72f8e9beb681de70f675354266731e429b22fa8
Jonescifer <wolf_paul@icloud.com>
2020-12-11 09:23:27 +0200

    instantiate script

--- CompleteCSharpMasterclass/_Unity/ZigZag Basics/Assets/InstantiateCubes.cs
+++ CompleteCSharpMasterclass/_Unity/ZigZag Basics/Assets/InstantiateCubes.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InstantiateCubes : MonoBehaviour
 {
     public Transform prefab;
+    private float _counter = 0;
     void Start()
     {
     //     for (int i = 0; i < 10; i++)
@@ -18,7 +19,8 @@ public class InstantiateCubes : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(prefab, new Vector3( 1, -1, 1 ), Quaternion.identity);
+            Instantiate(prefab, new Vector3( _counter * 1.666666666666667f, 0, 0 ), Quaternion.identity);
+            _counter++;
         };
     }
 }
