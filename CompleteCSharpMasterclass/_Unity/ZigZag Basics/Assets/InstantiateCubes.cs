a37b05dfcd8e37008fd1345e5492c41f364e8c96
Jonescifer <wolf_paul@icloud.com>
2020-12-11 10:51:42 +0200

    save data PlayerPrefs

--- CompleteCSharpMasterclass/_Unity/ZigZag Basics/Assets/InstantiateCubes.cs
+++ CompleteCSharpMasterclass/_Unity/ZigZag Basics/Assets/InstantiateCubes.cs
@@ -6,21 +6,19 @@ public class InstantiateCubes : MonoBehaviour
 {
     public Transform prefab;
     private float _counter = 0;
+
     void Start()
     {
-    //     for (int i = 0; i < 10; i++)
-    //     {
-    //         Instantiate(prefab, new Vector3(i * 1, i*-1, i*1 ), Quaternion.identity);
-    //     }
+        InvokeRepeating("CreateNewCube", 0,1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateNewCube()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(prefab, new Vector3( _counter * 1.666666666666667f, 0, 0 ), Quaternion.identity);
-            _counter++;
-        };
+        Instantiate(prefab, new Vector3( -10 + _counter * 1.666666666666667f, 0, 0 ), Quaternion.identity);
+        _counter++;
+        if (_counter >= 10)
+        { 
+            CancelInvoke();
+        }
     }
 }
---
+++ CompleteCSharpMasterclass/_Unity/ZigZag Basics/Assets/SavingData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SavingData : MonoBehaviour
+{
+    private int _numberCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"stored number is {GetNumber()}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _numberCounter++;
+            if (_numberCounter >= GetNumber())
+            {
+                PlayerPrefs.SetInt("score",_numberCounter);
+            }
+            Debug.Log($"the Number is {_numberCounter}");
+        }
+    }
+
+    private int GetNumber()
+    {
+        int score = PlayerPrefs.GetInt("score", 0);
+        return score;
+    }
+}
