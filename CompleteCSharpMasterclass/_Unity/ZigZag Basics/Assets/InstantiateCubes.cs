b32d3074a8c3cc11729a9d269f9c4d3e28c7eec5
Jonescifer <wolf_paul@icloud.com>
2020-12-11 09:00:09 +0200

    ZigZag Basics

---
+++ CompleteCSharpMasterclass/_Unity/ZigZag Basics/Assets/InstantiateCubes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+    public Transform prefab;
+    void Start()
+    {
+    //     for (int i = 0; i < 10; i++)
+    //     {
+    //         Instantiate(prefab, new Vector3(i * 1, i*-1, i*1 ), Quaternion.identity);
+    //     }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, new Vector3( 1, -1, 1 ), Quaternion.identity);
+        };
+    }
+}
