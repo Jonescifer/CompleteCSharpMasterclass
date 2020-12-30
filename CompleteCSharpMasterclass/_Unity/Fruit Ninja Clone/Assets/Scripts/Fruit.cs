08e90f82a258f17e10f5f4cb5faabf85ef8c8f5f
paulwolf <paul@inceptionxr.com>
2020-12-30 13:25:48 +0200

    first phase

---
+++ CompleteCSharpMasterclass/_Unity/Fruit Ninja Clone/Assets/Scripts/Fruit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedFruit;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
+    }
+
+
+    public void CreateSlicedFruit()
+    {
+        //instantiate the slice fruit prefab at the fruit's position.
+        var thisTransform = transform;
+        GameObject inst = (GameObject)Instantiate(slicedFruit, thisTransform.position, thisTransform.rotation);
+        
+        //Create rigidbody and explode!
+        Rigidbody[] rigidbodiesOnSliced = inst.transform.GetComponentInChildren<Rigidbody>();
+        
+        
+        
+        //destroy the fruit
+        Destroy(gameObject);
+        
+    }
+
+
+}
+    
